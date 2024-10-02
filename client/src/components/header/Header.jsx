3a9809f5ab47f0657b8b5a8ec02c92cff2850a15
Jay Prakash <jayp7808@gmail.com>
2024-10-02 14:29:07 +0530

    change in Header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -89,7 +89,7 @@ const Header = ({ type }) => {
             </h1>
             <p className="headerDesc">
               Get rewarded for your travels – unlock instant savings of 10% or
-              more with a free Lamabooking account
+              more with a free JayaBooking
             </p>
             {!user && 
               <>
