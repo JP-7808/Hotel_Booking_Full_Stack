8b911cb405d6643b9b06e903c97c02397115cca1
Jay Prakash <jayp7808@gmail.com>
2024-10-02 20:14:44 +0530

    change in index.js file

--- api/index.js
+++ api/index.js
@@ -1,40 +1,36 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import authRoute from "./routes/auth.js"
-import usersRoute from "./routes/users.js"
-import roomsRoute from "./routes/rooms.js"
-import hotelsRoute from "./routes/hotels.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import authRoute from './routes/auth.js';
+import usersRoute from './routes/users.js';
+import roomsRoute from './routes/rooms.js';
+import hotelsRoute from './routes/hotels.js';
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
 
-const connect = async() => {
+const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO);
-        console.log("Connected to mongoDb.")
+        console.log("Connected to MongoDB.");
     } catch (error) {
         throw error;
-        
     }
-}
+};
 
-mongoose.connection.on("disconnected", ()=>{
-    console.log("MongoDB disconnected")
-})
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected");
+});
 
-// middlewares
-app.use(cors(
-    {
-        origin: ['https://hotel-booking-full-stack-client.vercel.app', 'https://hotel-booking-full-stack-admin.vercel.app'],
-        methods:["POST", "GET", "PUT", "DELETE"],
-        credentials: true
-    }
-))
+// Middleware
+app.use(cors({
+    origin: ['https://hotel-booking-full-stack-client.vercel.app', 'https://hotel-booking-full-stack-admin.vercel.app'],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+}));
 
-// middlewares
 app.use(cookieParser());
 app.use(express.json());
 
@@ -43,7 +39,7 @@ app.use('/api/users', usersRoute);
 app.use('/api/hotels', hotelsRoute);
 app.use('/api/rooms', roomsRoute);
 
-app.use((err, req, res, next) =>{
+app.use((err, req, res, next) => {
     const errorStatus = err.status || 500;
     const errorMessage = err.message || "Something went wrong";
     return res.status(errorStatus).json({
@@ -54,8 +50,7 @@ app.use((err, req, res, next) =>{
     });
 });
 
-
 app.listen(8800, () => {
-    connect()
+    connect();
     console.log("Connected to backend");
-})
+});
